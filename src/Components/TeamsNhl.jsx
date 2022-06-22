d3015863da30efdc937d965275fe92f5e73793ef
Canario5 <48844307+Canario5@users.noreply.github.com>
2022-06-22 20:54:48 +0200

    cleanup

--- src/Components/TeamsNhl.jsx
+++ src/Components/TeamsNhl.jsx
@@ -1,10 +1,4 @@
-import { useState } from "react"
-
 export default function TeamsNhl() {
-	const [num, setNum] = useState(false)
-
-	const klikuj = () => setNum(true)
-
 	return (
 		<div
 			style={{ backgroundColor: "beige", color: "black", width: "100px" }}
