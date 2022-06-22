bccf853dcf1ac86d426ca69f4b6eef0f2d950a6c
Canario5 <48844307+Canario5@users.noreply.github.com>
2022-06-22 20:57:25 +0200

    Functions added

--- src/Components/GridHeadline.jsx
+++ src/Components/GridHeadline.jsx
@@ -1,4 +1,3 @@
-/* import { useEffect, useState } from "react" */
 import format from "date-fns/format"
 
 import HeadlineBox from "./HeadlineBox"
@@ -6,15 +5,6 @@ import HeadlineBox from "./HeadlineBox"
 import "./GridHeadline.css"
 
 export default function GridHeadline(props) {
-	/* const [dates, setDates] = useState([]) */
-	/* const [headlineBoxes, setHeadlineBoxes] = useState() */
-
-	/* 	generateBoxes() */
-	/* function addBox(number) {
-		setHeadlineBoxes(() => console.log(number))
-	}
-	<HeadlineBox className="headline-new" toggle={() => addBox(2)}></HeadlineBox> */
-
 	if (!props.dates) return
 	const headlineBoxes = props.dates?.map((day, i) => (
 		<HeadlineBox
@@ -26,9 +16,13 @@ export default function GridHeadline(props) {
 
 	return (
 		<div className="grid-headline">
-			<div className="grid-headliner grid-col-first">{"<"}</div>
+			<div onClick={props.prevDay} className="grid-headliner grid-col-first">
+				{"<"}
+			</div>
 			{headlineBoxes}
-			<div className="grid-headliner grid-col-last">{">"}</div>
+			<div onClick={props.nextDay} className="grid-headliner grid-col-last">
+				{">"}
+			</div>
 		</div>
 	)
 }
