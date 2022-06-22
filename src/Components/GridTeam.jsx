c25c7c363998b93fc9a5df0cf47f1780c988f024
Canario5 <48844307+Canario5@users.noreply.github.com>
2022-06-22 20:54:29 +0200

    Blank logos removed

--- src/Components/GridTeam.jsx
+++ src/Components/GridTeam.jsx
@@ -7,27 +7,25 @@ export default function GridTeam(props) {
 
 	const days = props.schedule.map((oneDay, i) => {
 		let homeAway = ""
-		oneDay < 9999 && gamesPlayed++
-		if (oneDay !== 9999 && oneDay > 0) homeAway = `grid-home`
-		if (oneDay !== 9999 && oneDay < 0) homeAway = `grid-away`
+		if (oneDay) gamesPlayed++
+
+		if (oneDay > 0) homeAway = `grid-home`
+		if (oneDay < 0) homeAway = `grid-away`
 
 		return (
 			<div
 				key={i}
 				className={`grid-team grid-col-${i} grid-${props.shortname} ${homeAway}`}
 			>
-				<img
-					src={logoNhl[Math.abs(oneDay)]}
-					className={`grid-logo grid-${props.shortname}`}
-				/>
+				{oneDay && (
+					<img
+						src={logoNhl[Math.abs(oneDay)]}
+						className={`grid-logo grid-${props.shortname}`}
+					/>
+				)}
 			</div>
 		)
 	})
-
-	/* function addBox(number) {
-		setHeadlineBoxes(() => console.log(number))
-	}
-	<HeadlineBox className="headline-new" toggle={() => addBox(2)}></HeadlineBox> */
 
 	return (
 		<div className={`grid-team-row grid-${props.shortname}`}>
