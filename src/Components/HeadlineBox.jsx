204154dc50b99a4563b8b0319fc3acb442ba48a7
Canario5 <48844307+Canario5@users.noreply.github.com>
2022-06-25 22:03:21 +0200

    del

--- src/Components/HeadlineBox.jsx
+++
@@ -1,9 +0,0 @@
-import "./HeadlineBox.css"
-
-export default function HeadlineBox(props) {
-	return (
-		<div className={props.className} onClick={() => console.log("Bambalam")}>
-			{props.dayDate}
-		</div>
-	)
-}
