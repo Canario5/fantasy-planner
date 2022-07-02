e0b2d17f88e5a01515de6a5a0992815f9f47e6d3
Canario5 <48844307+Canario5@users.noreply.github.com>
2022-07-03 01:12:02 +0200

    CSS .HalfWidth dark overlay effect

--- src/Styles/ExtraStyles.js
+++ src/Styles/ExtraStyles.js
@@ -1,5 +1,9 @@
 const HalfWidth = {
 	width: "50%",
+	filter: "saturate(40%) brightness(0.85)",
+
+	/* filter: "hue-rotate(300deg)", */
+	/* background: "rgba(0, 0, 0, .75)", */
 	/* transition: "width .15s", */
 }
 
